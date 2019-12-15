b17b641e5ad3138f991ad823296237827cf30a21
Ayush Kumar <k.ayush1998@gmail.com>
2019-12-15 17:11:12 +0530

    added question

--- competitive/lucky_minimum.py
+++ competitive/lucky_minimum.py
@@ -1,3 +1,30 @@
+"""
+Monk just purchased an array A having N integers. Monk is very superstitious. He calls the array A Lucky if the frequency of
+the minimum element is odd, otherwise he considers it Unlucky. Help Monk in finding out if the array is Lucky or not.
+
+Input:
+First line consists of a single integer T denoting the number of test cases.
+First line of each test case consists of a single integer N denoting the size of array A.
+Second line of each test case consists of N space separated integers denoting the array A.
+
+Output:
+For each test case, print "Lucky" (without quotes) if frequency of minimum element is odd, otherwise print "Unlucky"
+(without quotes). Print a new line after each test case.
+
+SAMPLE INPUT 
+2
+5
+8 8 9 5 9
+5
+3 3 3 5 3
+SAMPLE OUTPUT 
+Lucky
+Unlucky
+Explanation
+In first case, value of minimum element is 5 and it's frequency is 1 which is odd, so the array is Lucky.
+In second case, value of minimum element is 3 and it's frequency is 4 which is even, so the array is Unlucky.
+"""
+
 t=int(input())
 for x in range (0,t):
     N=int(input())
